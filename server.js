5061034e304495df44c265d6ded009741601b0a7
Szymon Dzialowski <sd@phase-ii.com>
2018-11-22 11:41:24 +0000

    doc

--- server.js
+++ server.js
@@ -341,9 +341,15 @@ if (staticServer && gc) {
     }
 
     fs.appendFileSync(file, `
+    
+// https://nodejs.org/api/http.html#http_class_http_serverresponse
+    
 const controller = (req, res, query = {}) => {
 
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    // res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+
+    res.statusCode = 404;
 
     res.end(JSON.stringify({
         page: {
